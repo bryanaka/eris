b7c3149b4d2568e1a76d3acb72316f56299f31f6
Bryan Robles <bryanaka0@gmail.com>
2020-06-21 16:43:42 -0700

    add db service methods

--- src/services/db.ts
+++ src/services/db.ts
@@ -1,5 +1,16 @@
-import { createConnection, Connection, ConnectionOptions } from 'typeorm';
+import {
+  createConnection,
+  Connection,
+  ConnectionOptions,
+  Repository,
+  ObjectType,
+  EntitySchema,
+  EntityManager,
+} from 'typeorm';
 import { DEFAULT_DB_OPTIONS } from 'eris/config/db';
+
+type RepositoryTarget<Entity> = ObjectType<Entity> | EntitySchema<Entity> | string;
+type TransactionCallback<T> = (entityManager: EntityManager) => Promise<T>;
 
 export default class DatabaseService {
   connection!: Connection;
@@ -9,22 +20,34 @@ export default class DatabaseService {
   }
 
   get isConnected(): boolean {
-    return !!this.connection;
+    return this.connection.isConnected;
   }
 
-  getRepo(): void {
-    return;
+  get entityManager(): EntityManager {
+    return this.connection.manager;
+  }
+
+  getRepository<Entity>(target: RepositoryTarget<Entity>): Repository<Entity> | Entity {
+    return this.connection.getRepository<Entity>(target);
+  }
+
+  getCustomRepository<Repo>(customRepo: ObjectType<Repo>): Repo {
+    return this.connection.getCustomRepository<Repo>(customRepo);
+  }
+
+  transaction<T>(transactionCallback: TransactionCallback<T>): Promise<T> {
+    return this.connection.transaction<T>(transactionCallback);
   }
 
   async connect(options: Partial<ConnectionOptions>): Promise<void> {
     try {
-      this.connection = await this._createConnection(options);
+      this.connection = await this.createConnection(options);
     } catch (err) {
       throw new Error(`Unable to connect to the database: ${err.message}`);
     }
   }
 
-  async _createConnection(options: Partial<ConnectionOptions> = {}): Promise<Connection> {
+  async createConnection(options: Partial<ConnectionOptions> = {}): Promise<Connection> {
     const connectionOptions = Object.assign({}, DEFAULT_DB_OPTIONS, options) as ConnectionOptions;
     return await createConnection(connectionOptions);
   }
