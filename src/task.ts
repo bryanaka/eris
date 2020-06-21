0a595ad78defbce6381fdacb8ef3b297c712552d
Bryan Robles <bryanaka0@gmail.com>
2020-06-21 15:33:31 -0700

    delete unnecessary files and - add tsconfig for typeorm - add path for db directory

--- src/task.ts
+++
@@ -1,9 +0,0 @@
-import { Client } from 'discord';
-
-export default abstract class DiscordTask {
-  client!: Client;
-
-  async execute(): Promise<void> {
-    throw new Error('you must define execute on the task class');
-  }
-}
